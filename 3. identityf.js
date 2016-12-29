90e97a474487601a15af6da8cdf803851e7217c3
BruceWeng <weng8916@gmail.com>
2016-12-28 17:25:39 -0800

    3. identityf.js

---
+++ 3. identityf.js
@@ -0,0 +1,14 @@
+// Write a function identityf that takes an argument and returns a function that returns that argument.
+// var three = identityf(3);
+// three() // 3
+function print(input) {
+  console.log(input);
+}
+function identityf(id) {
+  return function () {
+    return id;
+  }
+}
+
+var three = identityf(3);
+print(three());
