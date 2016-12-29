4a266e9600d44bd2352c43e27bb75419640bb963
BruceWeng <weng8916@gmail.com>
2016-12-28 17:18:20 -0800

    1. identity.js

---
+++ 1. identity.js
@@ -0,0 +1,11 @@
+// Write an identity function that takes an argument and return the argument.
+// identity(3) -> 3
+function identity(input) {
+  return input;
+}
+
+function print(input) {
+  console.log(input);
+}
+
+print(identity(3));
