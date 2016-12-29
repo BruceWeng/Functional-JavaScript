3777ef84a7c720831fdec8c202f86e4de15304be
BruceWeng <weng8916@gmail.com>
2016-12-28 17:21:49 -0800

    2. cal.js

---
+++ 2. cal.js
@@ -0,0 +1,23 @@
+// Write three binary functions add, sub and mul that take two numbers and return their sum, difference and product.
+// add(3, 4) -> 7
+// sub(3, 4) -> -1
+// mul(3, 4) -> 12
+function print(input) {
+  console.log(input);
+}
+
+function add(first, second) {
+  return first + second;
+}
+
+function sub(first, second) {
+  return first - second;
+}
+
+function mul(first, second) {
+  return first * second;
+}
+
+print(add(3, 4));
+print(sub(3, 4));
+print(mul(3, 4));
