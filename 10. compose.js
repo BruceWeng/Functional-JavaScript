35054a368bd24e9d873418fafbaa95b2ea9aff03
BruceWeng <weng8916@gmail.com>
2017-01-03 15:26:28 -0800

    10. compose.js

---
+++ 10. compose.js
@@ -0,0 +1,23 @@
+// Write a curry function compose that takes two unary functions and returns a unary function that calls them both.
+// 	composeu(doubl) (square) (5) // 50
+function print(input) {
+  console.log(input);
+}
+
+function doubl(val) {
+  return val * 2;
+}
+
+function square(val) {
+  return val * val;
+}
+
+function compose(unary1) {
+  return function(unary2) {
+    return function (val) {
+      return unary1(unary2(val));
+    }
+  }
+}
+
+print(compose(doubl) (square) (5));
