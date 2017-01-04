ca68e6c86cf1a3846aa8e3ce4195fdb58db59bbe
BruceWeng <weng8916@gmail.com>
2017-01-03 16:01:26 -0800

    11. square.js

---
+++ 11. square.js
@@ -0,0 +1,16 @@
+// Write a function square that takes two arrays and returns an array with square of each elements.
+// 	square([1, 3, 5],[2, 4, 6])// [1, 9, 25, 4, 16, 36]
+
+function print(input) {
+  console.log(input);
+}
+
+function square(...numbers) {
+  return numbers.map(function (number) {
+    return number * number;
+  });
+}
+
+var numbers1 = [1, 3, 5];
+var numbers2 = [2, 4, 6];
+print(square(...numbers1, ...numbers2));
